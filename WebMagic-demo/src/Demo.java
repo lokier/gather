db52d77027c9a744bfe122b20250c012afa6bbf6
lokierao(饶东铭) <lokierao@tencent.com>
2018-11-27 12:51:47 +0800

    init project

---
+++ WebMagic-demo/src/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+
+	/**
+	 * 中文: http://webmagic.io/docs/zh/
+English: http://webmagic.io/docs/en
+Javadocs: http://webmagic.io/apidocs/
+0.6.1版文档: http://webmagic.io/archive/docs/0.6.1
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
